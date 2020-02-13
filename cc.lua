052471e308404c95bb4ed28551bab63fb4e1711d
XToolBox <admin@xtoolbox.org>
2020-02-13 21:21:24 +0800

    Compute require count at run time
    Table in lua is not stable, so compute the require count when require them,

--- cc.lua
+++ cc.lua
@@ -1,30 +1,16 @@
 local old_require = _G.require
-local req_tree = {}
-local cur_req = req_tree
-local collect_require = function(x)
-    local last_req = cur_req
-    cur_req[x] = cur_req[x] or {}
-    cur_req = cur_req[x]
-    local r = old_require(x)
-    cur_req = last_req
-    return r
-end
-_G.require = collect_require
-require(arg[1])
 local counts = {}
-local files = {}
-function count_req(t)
-    local r = 1
-    for k,v in pairs(t) do
-        local c = counts[k] or count_req(v)
-        if not counts[k] then
-            files[#files+1] = k
-            counts[k] = c
-        end
-        r = r + c
-    end
+local current_count = 0
+_G.require = function(x)
+	local last_count = current_count
+	current_count = 1
+    local r = old_require(x)
+	counts[x] = counts[x] or current_count
+	current_count = last_count + counts[x]
     return r
 end
-count_req(req_tree)
+require(arg[1])
+local files = {}
+for k,v in pairs(counts) do files[#files+1] = k end
 table.sort(files, function(v1,v2)  return counts[v1]<counts[v2] end)
 os.execute("luac "..table.concat(files, ".lua ")..".lua")
