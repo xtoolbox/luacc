fc83034cd360547d76afa94f813fbbf8cea0327b
xtoolbox <admin@xtoolbox.org>
2021-02-14 22:33:49 +0800

    replace tab to space

--- cc.lua
+++ cc.lua
@@ -14,19 +14,19 @@ local files = {}
 for k,v in pairs(counts) do files[#files+1] = k end
 table.sort(files, function(v1,v2)  return counts[v1]<counts[v2] end)
 if arg[2] then
-	local macro_tb = {}
-	for k,v in pairs(require(arg[2])) do
-		macro_tb[arg[2] .. "." .. k] = v
-	end
-	os.execute("mkdir cc_temp")
-	for i,v in ipairs(files) do
-		local tfile = io.open("cc_temp/" .. v .. ".lua", "w+")
-		for l in io.lines(v..".lua") do
-			tfile:write(string.gsub(l,"("..arg[2]..".[A-Z_]+)", macro_tb), "\n")
-		end
-		tfile:close()
-	end
-	os.execute("luac cc_temp/"..table.concat(files, ".lua cc_temp/")..".lua")
+    local macro_tb = {}
+    for k,v in pairs(require(arg[2])) do
+        macro_tb[arg[2] .. "." .. k] = v
+    end
+    os.execute("mkdir cc_temp")
+    for i,v in ipairs(files) do
+        local tfile = io.open("cc_temp/" .. v .. ".lua", "w+")
+        for l in io.lines(v..".lua") do
+            tfile:write(string.gsub(l,"("..arg[2]..".[A-Z_]+)", macro_tb), "\n")
+        end
+        tfile:close()
+    end
+    os.execute("luac cc_temp/"..table.concat(files, ".lua cc_temp/")..".lua")
 else
-	os.execute("luac "..table.concat(files, ".lua ")..".lua")
+    os.execute("luac "..table.concat(files, ".lua ")..".lua")
 end
